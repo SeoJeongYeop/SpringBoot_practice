3ee02ae0011656e608c747d81cf463bb3c436cc6
SeoJeongYeop <sjyswe99@gmail.com>
2022-12-17 15:26:55 +0900

    init: Add spring boot initializer project

---
+++ src/main/java/com/hello/skkuspringboota1/SkkuSpringBootA1Application.java
@@ -0,0 +1,13 @@
+package com.hello.skkuspringboota1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkkuSpringBootA1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkkuSpringBootA1Application.class, args);
+	}
+
+}
